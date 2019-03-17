1b449cebdd21e1621ec6a6be79e8698fa37acc5d
Ashley Radford <aradford7@outlook.com>
2019-03-17 06:13:52 -0700

    Update main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,35 @@
-console.log("Up and Running!");
-var cards = ["queen", "queen", "king", "king"];
+//console.log("Up and Running!");
 
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",}
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",}
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",}
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds",
+}
+];
 
  var cardsInPlay = [];
  var checkForMatch = function() {
+ 	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 }	else {
 	console.log("Sorry, try again.");
 }
 
-var flipCard = function() {
-	var cardId =
+var flipCard = function(cardId) {
 	console.log("User flipped" + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 }
